d54b9561ceca6ebdc7f98e9510385fdc2e5a22e4
A2N2 <129242580+atoonatoo@users.noreply.github.com>
2024-08-30 15:23:15 +0900

    first commit

---
+++ src/main/java/com/mbti/mbti/MbtiTextApplication.java
@@ -0,0 +1,13 @@
+package com.mbti.mbti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtiTextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MbtiTextApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mbti/mbti/MbtiTextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbti.mbti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtiTextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
